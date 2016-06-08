ac11c4840804eaa81e7b8a369377ade63325e166
Lowell Bander <lowellbander@gmail.com>
2016-06-07 19:40:51 -0700

    repositioning

--- src/Serialization.js
+++ src/Serialization.js
@@ -6,7 +6,7 @@ class Serialization {
     
     constructor(context) {
         this.x = 10;
-        this.y = 250;
+        this.y = 275;
         this.lineOffset = 15;
         this.context = context;
         this.context.fillStyle = 'black';
